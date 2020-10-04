7d8360ba499648f5d7184b48e332014b6c9595ff
Passeriform <utbh23041999@gmail.com>
2020-10-04 20:35:12 +0530

    Docs: Document app module (testing out docstyle).

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,59 +1,64 @@
+/// Angular imports
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 
+// JQuery // TODO: Eliminate
 import * as $ from 'jquery';
 
-import { FetchService } from './services/fetch.service';
-import { SplashStateService } from './services/splash-state.service';
-import { TaggerService } from './services/tagger.service';
-import { WorkStateService } from './services/work-state.service';
-
+/// App component and routing
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
+
+/// Routable Pages
+import { LandingComponent } from './landing/landing.component';
+import { ExploreComponent } from './explore/explore.component';
+import { AboutComponent } from './about/about.component';
+
+/// Components (In orders of specificity, relationship)
+import { ScrollableComponent } from './scrollable/scrollable.component';
 
 import { HeaderComponent } from './header/header.component';
 
 import { NavtabComponent } from './navtab/navtab.component';
 import { NavtabDirective } from './navtab/navtab.directive';
 
-import { ScrollableComponent } from './scrollable/scrollable.component';
-
-import { LandingComponent } from './landing/landing.component';
-
-import { ShowcaseComponent } from './showcase/showcase.component';
-
-import { ExploreComponent } from './explore/explore.component';
-
-import { DescribeComponent } from './describe/describe.component';
-
-import { AboutComponent } from './about/about.component';
-
-import { SweeperComponent } from './sweeper/sweeper.component';
-
 import { CageGridComponent } from './cage-grid/cage-grid.component';
 import { CageGridDirective } from './cage-grid/cage-grid.directive';
 
-import { ListGroupComponent } from './listgroup/listgroup.component';
-
 import { TooltipComponent } from './tooltip/tooltip.component';
 
-import { OverlayComponent } from './overlay/overlay.component';
-import { DynamicSearchComponent } from './dynamic-search/dynamic-search.component';
+import { ListGroupComponent } from './listgroup/listgroup.component';
 
-import { SocialGlyphsComponent } from './social-glyphs/social-glyphs.component';
+import { OverlayComponent } from './overlay/overlay.component';
 
 import { RaisecardComponent } from './raisecard/raisecard.component';
+
+// Landing
+import { SweeperComponent } from './sweeper/sweeper.component';
+
+// Explore
+import { ShowcaseComponent } from './showcase/showcase.component';
+
+import { DescribeComponent } from './describe/describe.component';
+
+import { DynamicSearchComponent } from './dynamic-search/dynamic-search.component';
+
+// About
+import { SocialGlyphsComponent } from './social-glyphs/social-glyphs.component';
+
+/// Services
+import { FetchService } from './services/fetch.service';
+import { SplashStateService } from './services/splash-state.service';
+import { TaggerService } from './services/tagger.service';
+import { WorkStateService } from './services/work-state.service';
+
+/// Pipes
 import { IconUriPipe } from './icon-uri.pipe';
 import { RetainPipe } from './retain.pipe';
 
-// import { TreeComponent } from './tree/tree.component';
-// import { NodeComponent } from './tree/node.component';
-// import { PivotnavComponent } from './pivotnav/pivotnav.component';
-// import { PItemComponent } from './pivotnav/pitem.component';
-// import { CircularWaveComponent } from './circular-wave/circular-wave.component';
-
+/// Module registration
 @NgModule({
   imports: [
     FormsModule,
@@ -86,6 +91,7 @@ import { RetainPipe } from './retain.pipe';
     CageGridComponent,
     CageGridDirective,
 
+    ListGroupComponent,
 
     TooltipComponent,
 
@@ -109,4 +115,5 @@ import { RetainPipe } from './retain.pipe';
   ],
   bootstrap: [AppComponent]
 })
+
 export class AppModule { }
