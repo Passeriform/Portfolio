22981852cbdc0e8b72debe5c22a524e804b05cb3
Passeriform <utbh23041999@gmail.com>
2021-02-01 09:22:45 +0530

    MapExternalPipe: Remove redundant global wikiUrl storage.
    Querying from Wikipedia API now.

--- src/app/map-external.pipe.ts
+++ src/app/map-external.pipe.ts
@@ -18,30 +18,30 @@ action=query
 &origin=*
 &prop=info|description
 &formatversion=2
-&inprop=
+&inprop=url
 &titles=${this.getWikiTitle(entity)}`;
 
-    // const callUrl = `https://en.wikipedia.org/w/api.php?
-    //   action=query
-    //   &format=json
-    //   &origin=*
-    //   &prop=extracts
-    //   &formatversion=2
-    //   &exlimit=1
-    //   &exintro=1
-    //   &explaintext=1
-    //   &titles=${getWikiTitle(entity)}`;
+//     const callUrl = `https://en.wikipedia.org/w/api.php?
+// action=query
+// &format=json
+// &origin=*
+// &prop=extracts
+// &formatversion=2
+// &exlimit=1
+// &exintro=1
+// &explaintext=1
+// &titles=${getWikiTitle(entity)}`;
 
-    // const callUrl = `https://en.wikipedia.org/w/api.php?
-    //   format=json
-    //   &origin=*
-    //   &action=query
-    //   &list=search
-    //   &srlimit=1
-    //   &redirects=1
-    //   &srsearch=${srtitle}_${srcategory}`;
+//     const callUrl = `https://en.wikipedia.org/w/api.php?
+// format=json
+// &origin=*
+// &action=query
+// &list=search
+// &srlimit=1
+// &redirects=1
+// &srsearch=${srtitle}_${srcategory}`;
 
-    // TODO: Add a wikipedia query model to replace <any> type
+    // TODO: Use generator API to fetch proper description as well.
     // TODO: Consider switching to google search results and picking the first result instead
     // TODO: Fix 404 on search results
 
@@ -50,11 +50,12 @@ action=query
         const page = (response.query ? response.query.pages : null || [{}])[0];
         const title = page.title || '';
         const description = page.description || '';
+        const href = page.fullurl || '#';
 
         return {
           title,
           description,
-          href: this.getHref(entity)
+          href
         };
       }),
       catchError((error) => {
@@ -68,14 +69,6 @@ action=query
     for (const entry of registry) {
       if (entry.identifier === EntityIdentifier[identifier]) {
         return entry.wikiTitle;
-      }
-    }
-  }
-
-  getHref(identifier: string) {
-    for (const entry of registry) {
-      if (entry.identifier === EntityIdentifier[identifier]) {
-        return entry.href || '#';
       }
     }
   }
