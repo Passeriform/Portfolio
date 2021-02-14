e78278d970c4dcaa574ef6c710ba6c5fd0585108
Passeriform <utbh23041999@gmail.com>
2021-02-15 03:29:43 +0530

    Typing: Add dedicated GithubEvent types.

--- src/app/landing/update-roll/github.interface.ts
+++ src/app/landing/update-roll/github.interface.ts
@@ -11,14 +11,154 @@ export interface GithubRepo {
 	url: string;
 }
 
-// TODO: Model payload depending on event-type
 export interface GithubEvent {
 	id: number;
 	type: string;
 	actor: GithubUser;
 	repo: GithubRepo;
-	// Generic payload must be deserialized independently
-	payload: any;
+	payload?:
+	| object
+	| CommitCommentEventPayload
+	| CreateDeleteEventPayload
+	| ForkEventPayload
+	| GollumEventPayload
+	| IssueCommentEventPayload
+	| IssuesEventPayload
+	| MemberEventPayload
+	| PullRequestEventPayload
+	| PullRequestReviewCommentEventPayload
+	| PushEventPayload
+	| ReleaseEventPayload
+	| SponsorshipEventPayload
+	| WatchEventPayload;
 	public: boolean;
 	created_at: string;
+}
+
+interface CommitCommentEventPayload {
+	action: string;
+	comment: object;
+}
+
+interface CreateDeleteEventPayload {
+	ref: string;
+	ref_type: string;
+	master_branch?: string;
+	description?: string;
+}
+
+interface ForkEventPayload {
+	forkee: object;
+}
+
+interface GollumEventPayload {
+	pages: {
+		page_name: string;
+		title: string;
+		action: string;
+		sha: string;
+		html_url: string;
+	}[];
+}
+
+interface IssueCommentEventPayload {
+	action: string;
+	changes: {
+		body: {
+			from: string;
+		};
+	};
+	issue: object;
+	comment: object;
+}
+
+interface IssuesEventPayload {
+	action: string;
+	issue: object;
+	changes: {
+		title: { from: string };
+		body: { from: string };
+	};
+	assignee: object;
+	label: object;
+}
+
+interface MemberEventPayload {
+	action: string;
+	member: object;
+	changes: {
+		old_permission: {
+			from: string;
+		};
+	};
+}
+
+interface PullRequestEventPayload {
+	action: string;
+	number: number;
+	changes: {
+		title: { from: string };
+		body: { from: string };
+	};
+	pull_request: object;
+}
+
+interface PullRequestReviewCommentEventPayload {
+	action: string;
+	changes: {
+		body: {
+			from: string;
+		};
+	};
+	pull_request: object;
+	comment: object;
+}
+
+interface PushEventPayload {
+	push_id: number;
+	size: number;
+	distinct_size: number;
+	ref: string;
+	head: string;
+	before: string;
+	commits: {
+		sha: string;
+		message: string;
+		author: {
+			name: string;
+			email: string;
+		};
+		url: string;
+		distinct: boolean;
+	}[];
+}
+
+interface ReleaseEventPayload {
+	action: string;
+	changes: {
+		body: {
+			from: string;
+		};
+		name: {
+			from: string;
+		};
+		release: object;
+	};
+}
+
+interface SponsorshipEventPayload {
+	action: string;
+	effective_date: string;
+	changes: {
+		tier: {
+			from: object;
+		};
+		privacy_level: {
+			from: string;
+		};
+	};
+}
+
+interface WatchEventPayload {
+	action: string;
 }
