ec467e041b9e1e4d5efa5182ff6193e03274f2e9
Passeriform <utbh23041999@gmail.com>
2023-01-09 09:15:09 +0530

    Header: Use splashState as observable in template.

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,6 +1,8 @@
 import type { OnInit } from "@angular/core";
 import { Component, Input } from "@angular/core";
 
+import type { Observable } from "rxjs";
+
 import { SplashState } from "@core/services/splash-state.interface";
 import { SplashStateService } from "@core/services/splash-state.service";
 
@@ -15,24 +17,21 @@ export class HeaderComponent implements OnInit {
 	@Input() public readonly target: string;
 
 	public readonly SplashState = SplashState;
-	public splashState: SplashState;
+	public splashState$: Observable<SplashState>;
 
 	// TODO: Add accent color border-bottom based on page theme.
 
 	constructor(private readonly splashStateService: SplashStateService) { }
 
 	ngOnInit() {
-		this.splashStateService.splashState$.subscribe(
-			(splashState: SplashState) => {
-				this.splashState = splashState;
-
-				document.documentElement.style.setProperty(
-					"--current-header-shift-offset",
-					this.splashState === SplashState.BLURRED
-						? "var(--shrink-header-size-em, 4em)"
-						: "0"
-				);
-			},
-		);
+		this.splashState$ = this.splashStateService.splashState$;
+		this.splashState$.subscribe((splashState: SplashState) => {
+			document.documentElement.style.setProperty(
+				"--current-header-shift-offset",
+				splashState === SplashState.BLURRED
+					? "var(--shrink-header-size-em, 4em)"
+					: "0",
+			);
+		});
 	}
 }
