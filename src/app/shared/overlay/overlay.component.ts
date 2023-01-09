abcec26f930d6f7cef9b0abd8fd4a40dc1b08813
Passeriform <utbh23041999@gmail.com>
2023-01-09 07:52:57 +0530

    Overlay: Update interface and format code.

--- src/app/shared/overlay/overlay.component.ts
+++ src/app/shared/overlay/overlay.component.ts
@@ -1,36 +1,20 @@
-import type { OnInit } from "@angular/core";
 import { Component, EventEmitter, HostBinding, Output } from "@angular/core";
 
-import type { WorkModel } from "@app/work/work.interface";
-import { WorkService } from "@app/work/services/work.service";
-
-enum OverlayState {
-	HIDE,
-	SHOW,
-}
+import { OverlayState } from "./overlay.config";
 
 @Component({
 	selector: "app-overlay",
 	styleUrls: [ "./overlay.component.scss" ],
 	templateUrl: "./overlay.component.html",
 })
-export class OverlayComponent implements OnInit {
-	@Output() public readonly triggerEvent: EventEmitter<void> = new EventEmitter<void>();
-
-	public readonly OverlayState = OverlayState;
+export class OverlayComponent {
 	public overlayState: OverlayState;
 
+	public readonly OverlayState = OverlayState;
+
+	@Output() public readonly triggerEvent: EventEmitter<void> = new EventEmitter<void>();
+
 	@HostBinding("class.blink") public blinkEnabled = true;
-
-	constructor(private readonly workService: WorkService) { }
-
-	ngOnInit() {
-		this.workService.workSelectedState$.subscribe((model?: WorkModel) => {
-			if (model) {
-				this.blinkEnabled = false;
-			}
-		});
-	}
 
 	public toggleOverlay(): void {
 		this.overlayState = this.overlayState === OverlayState.SHOW
@@ -38,21 +22,13 @@ export class OverlayComponent implements OnInit {
 			: OverlayState.SHOW;
 	}
 
-	public showOverlay(): void {
-		this.overlayState = OverlayState.SHOW;
-	}
-
 	public hideOverlay(): void {
 		this.overlayState = OverlayState.HIDE;
 	}
 
-	public disableBlink(): void {
-		this.blinkEnabled = false;
-	}
-
 	public handleButtonClick(): void {
 		this.toggleOverlay();
-		this.disableBlink();
+		this.blinkEnabled = false;
 		this.triggerEvent.emit();
 	}
 }
