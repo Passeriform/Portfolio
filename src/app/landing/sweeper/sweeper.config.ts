8b779e3bd6201858b35d1d94002aa131c1c4cd1c
Passeriform <bhardwajutkarsh.ub@gmail.com>
2023-11-28 01:51:07 +0530

    Sweeper: Add highlight pseudo elements to sweeper container.

--- src/app/landing/sweeper/sweeper.config.ts
+++ src/app/landing/sweeper/sweeper.config.ts
@@ -1,5 +1,7 @@
-export const Constants: {
-	readonly INITIAL_DELAY: number;
-} = {
+export const Constants: Readonly<{
+	HIGHLIGHT_PADDING_FACTOR: number;
+	INITIAL_DELAY: number;
+}> = {
+	HIGHLIGHT_PADDING_FACTOR: 1.33,
 	INITIAL_DELAY: 5000,
 };
