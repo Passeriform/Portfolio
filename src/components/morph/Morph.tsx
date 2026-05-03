8c67eac2bd0046d6d36774cf6cedd86da6b9824f
Passeriform <bhardwajutkarsh.ub@gmail.com>
2026-05-03 07:23:45 +0530

    [Component] Add helper component for morphing between texts

---
+++ src/components/morph/Morph.tsx
@@ -0,0 +1,91 @@
+import { Children, cloneElement, isValidElement, useEffect, useRef, useState, type PropsWithChildren, type ReactElement, type Ref } from "react";
+import FilterSVG from "./morph.svg?react"
+import "./Morph.css";
+
+type MorphProps = ({
+    on: "hover"
+} | {
+    on: "interval"
+    after: number
+}) & { duration: number, strength: number | number[] }
+
+export const Morph = ({ children, ...schedule }: PropsWithChildren<MorphProps>) => {
+    const [activeIndex, setActiveIndex] = useState(0)
+    const containerRef = useRef<HTMLDivElement>(null)
+    const childrenRefs = useRef<(HTMLElement | null)[]>([])
+
+    useEffect(() => {
+        if (schedule.on !== "interval") {
+            return
+        }
+
+        let startTimestamp: number
+        let requestId: number
+
+        const intervalHandler = (now: number) => {
+            if (!startTimestamp) {
+                startTimestamp = now
+            }
+            if ((now - startTimestamp) >= schedule.after) {
+                setActiveIndex((currentIndex) => (currentIndex + 1) % childrenRefs.current.length)
+                startTimestamp = 0
+            }
+            requestId = requestAnimationFrame(intervalHandler)
+        }
+
+        requestId = requestAnimationFrame(intervalHandler)
+
+        return () => cancelAnimationFrame(requestId)
+    }, [...Object.values(schedule)])
+
+    useEffect(() => {
+        containerRef.current?.style.setProperty("--duration", `${schedule.duration}`)
+    }, [schedule.duration])
+
+    useEffect(() => {
+        containerRef.current?.style.setProperty("--strength", `${
+            typeof schedule.strength === "number" ?
+                schedule.strength :
+                schedule.strength[(activeIndex + childrenRefs.current.length - 1) % childrenRefs.current.length]
+        }`)
+    }, [schedule.strength, activeIndex])
+
+    return <div className="morphContainer"
+        onMouseEnter={() => schedule.on === "hover" && setActiveIndex(1)}
+        onMouseOut={() => schedule.on === "hover" && setActiveIndex(0)}
+        ref={containerRef}
+    >
+        <>{Children.map(children, (child, index) => {
+            if (!isValidElement(child)) {
+                return child
+            }
+
+            const element = child as ReactElement<{
+                className?: string
+                ref?: Ref<HTMLElement>
+            }>
+
+            return cloneElement(element, {
+                className: `${element.props.className || ""} ${index === activeIndex ? "active" : ""} morphText`.trim(),
+                ref: (node) => {
+                    childrenRefs.current[index] = node
+
+                    const originalRef = element.props.ref
+
+                    if (!originalRef) {
+                        return
+                    }
+
+                    if (typeof originalRef === "function") {
+                        originalRef(node)
+                    } else if (originalRef && "current" in originalRef) {
+                        originalRef.current = node
+                    }
+                }
+            });
+        })}</>
+        <FilterSVG className="hidden" />
+    </div>
+}
+
+export default Morph
