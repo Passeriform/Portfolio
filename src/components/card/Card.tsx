eb1d368bf6389a19dc9da34eb1d2f1440408e527
Passeriform <bhardwajutkarsh.ub@gmail.com>
2026-05-05 09:39:50 +0530

    [Component] Update card to add decorations via SVG

--- src/components/card/Card.tsx
+++ src/components/card/Card.tsx
@@ -2,16 +2,21 @@ import { type PropsWithChildren, useEffect, useLayoutEffect, useRef, useState } 
 import classes from "./Card.module.css"
 
 type CardProps = {
+    accentSize?: number
+    accentColor?: `#${string}`
     borderSize?: number
+    borderColor?: `#${string}`
     clipAt?: number
 }
 
-export const Card = ({ borderSize = 1, clipAt = 20, children }: PropsWithChildren<CardProps>) => {
-    const cardContainer = useRef<HTMLDivElement>(null)
+export const Card = ({ accentSize = 2, borderSize = 1, borderColor = "#737373", accentColor = "#dedede", clipAt = 20, children }: PropsWithChildren<CardProps>) => {
+    const content = useRef<HTMLDivElement>(null)
+    const decoration = useRef<HTMLDivElement>(null)
     const [dimensions, setDimensions] = useState<[number, number]>([0, 0])
+    const [show, setShow] = useState(false)
 
     useLayoutEffect(() => {
-        const element = cardContainer.current
+        const element = decoration.current
 
         if (!element) {
             return
@@ -33,31 +38,44 @@ export const Card = ({ borderSize = 1, clipAt = 20, children }: PropsWithChildre
 
     useEffect(() => {
         setTimeout(() => {
-            cardContainer.current?.classList.add(classes.show)
+            setShow(true)
         }, 2000)
-        cardContainer.current?.style.setProperty("--border-size", `${borderSize}px`)
+        decoration.current?.style.setProperty("--clip-at", `${clipAt}`)
     }, [])
 
-    const [width, height] = dimensions.map((coordinate) => Math.floor(coordinate))
+    if (accentSize < borderSize) {
+        throw Error("accentSize must be larger than borderSize")
+    }
+
+    const [width, height] = dimensions
 
     return (
-        <div className={classes.cardContainer} ref={cardContainer}>
-            <div className={classes.contentContainer}>
-                {width > 0 && (
-                    <svg viewBox={`0 0 ${width} ${height}`} className="hidden">
-                        <g fill="none" stroke="currentColor" strokeWidth={borderSize}>
-                            <path
-                                d={`M${borderSize / 2} ${height - clipAt} L${borderSize / 2} ${borderSize / 2} L${width - clipAt} ${borderSize / 2}`}
-                            />
-                            <path
-                                d={`M${width - borderSize} ${clipAt} L${width - borderSize} ${height - borderSize} L${clipAt} ${height - borderSize}`}
-                            />
-                        </g>
-                    </svg>
-                )}
+        <div className={classes.container}>
+            <div className={classes.content} ref={content}>
                 {children}
             </div>
+            <div className={`${classes.decoration} ${show ? classes.show : ""}`} ref={decoration}>
+                <svg viewBox={`${width + accentSize < clipAt ? width - clipAt : -accentSize} ${height + accentSize < clipAt ? height - clipAt : -accentSize} ${Math.max(width + 2 * accentSize, 2 * clipAt)} ${Math.max(height + 2 * accentSize, 2 * clipAt)}`} vectorEffect="non-scaling-stroke">
+                    {(width >= clipAt && height >= clipAt) && <g fill="none" stroke={`${borderColor}`} strokeWidth={borderSize}>
+                        <path
+                            d={`M0 ${height - clipAt} L0 0 L${width - clipAt} 0`}
+                        />
+                        <path
+                            d={`M${width} ${clipAt} L${width} ${height} L${clipAt} ${height}`}
+                        />
+                    </g>}
+                    <g fill="none" stroke={`${accentColor}`} strokeWidth={accentSize}>
+                        <path
+                            d={`M0 ${clipAt} L0 0 L${clipAt} 0`}
+                        />
+                        <path
+                            d={`M${width} ${height - clipAt} L${width} ${height} L${width - clipAt} ${height}`}
+                        />
+                    </g>
+                </svg>
+            </div>
         </div>
+            
     )
 }
 
